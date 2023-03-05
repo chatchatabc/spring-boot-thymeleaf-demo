8904bdb0251687947687ebb3115925ef8c85705e
Libing Chen <libing.chen@gmail.com>
2023-03-05 12:52:48 +0800

    feat: add HTMX request

--- src/main/java/org/mvnsearch/thymeleaf/demo/HomeController.java
+++ src/main/java/org/mvnsearch/thymeleaf/demo/HomeController.java
@@ -1,5 +1,7 @@
 package org.mvnsearch.thymeleaf.demo;
 
+import io.github.wimdeblauwe.hsbt.mvc.HtmxRequest;
+import io.github.wimdeblauwe.hsbt.mvc.HxRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,5 +14,11 @@ public class HomeController {
     public String index(Model model) {
         model.addAttribute("user", "Admin");
         return "index";
+    }
+
+    @GetMapping("/hello")
+    @HxRequest
+    public String hello(HtmxRequest htmxRequest, Model model) {
+        return "partials/hello";
     }
 }
