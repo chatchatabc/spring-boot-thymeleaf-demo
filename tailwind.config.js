019eac71988835422f7cedea25aa75fa37308fb8
Libing Chen <libing.chen@gmail.com>
2023-03-06 15:28:04 +0800

    chore: add  './src/main/java/**/*.html'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/main/resources/templates/**/*.html', './src/main/kotlin/**/*.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/main/resources/templates/**/*.html', './src/main/kotlin/**/*.html', './src/main/java/**/*.html'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
