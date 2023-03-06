bef9457ebd90dc8722476a417db775a1f1a4479f
Libing Chen <libing.chen@gmail.com>
2023-03-06 15:26:16 +0800

    chore: api adjusted

--- src/main/kotlin/org/mvnsearch/thymeleaf/demo/components/HelloComponent.kt
+++ src/main/kotlin/org/mvnsearch/thymeleaf/demo/components/HelloComponent.kt
@@ -1,11 +1,12 @@
 package org.mvnsearch.thymeleaf.demo.components
 
 import de.tschuehly.thymeleafviewcomponent.ViewComponent
-import de.tschuehly.thymeleafviewcomponent.ViewComponentContext
+import de.tschuehly.thymeleafviewcomponent.ViewContext
+import de.tschuehly.thymeleafviewcomponent.ViewProperty
 
 @ViewComponent
 class HelloComponent {
-    fun render(): ViewComponentContext {
-        return ViewComponentContext("HelloComponent", Pair("user", "Admin"))
+    fun render(): ViewContext {
+        return ViewContext("HelloComponent", ViewProperty("user", "Admin"))
     }
 }
